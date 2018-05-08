ba4d8d09e445bae7dc1916c43aa1ffd9ea97249b
Olivia Herzog <olivia-herzog@hotmail.com>
2018-05-08 16:55:42 +0200

    d16

--- node/server-loadWL.js
+++ node/server-loadWL.js
@@ -34,31 +34,128 @@ var loadWLData = function(url){
     });
   });
 }
+
+  var parseCSV = function(body){
+    return new Promise( function( resolve, reject){
+      csv.parse(body, {delimiter:';'}, function(err, data){
+      if(err) {reject()}
+      else {resolve(data) }
+      });
+
+    });
+  }
+
+var saveWLJSON = function (linien, steige, haltestellen){
+  return new Promise( function( resolve, reject){
+
+    var ubahnen = {}; //alle Infos in einem Objekt zusammengeführt
+
+    for( let i in linien){
+      if ( linien[i][4] != 'ptMetro') continue;
+
+      //UBahn gefunden und jetzt?
+      ubahnen[ linien[i][1]] = {
+        id: linien[i][0]*1,
+        haltestellen: [],
+      };
+
+      for( let j in steige){
+        if( steige[j][1] != linien[i][0]) continue;
+
+        //überprüfe ob haltestelle schon existiert (da es immer 2 steige gibt)
+        var hsExists = -1;
+        for(let k in ubahnen[ linien[i][1]].haltestellen) {
+          if(ubahnen[linien[i][1] ].haltestellen[k].id == steige[j][2]) {
+            hsExists = k;
+            break;
+          }
+        }
+        //passender bahnsteig was jetzt?
+        if(hsExists == -1){
+          ubahnen[linien[i][1] ].haltestellen.push( {
+            id:steige[j][2]
+          });
+          hsExists = ubahnen[linien[i][1] ].haltestellen.length -1;
+          for (let l in haltestellen){
+            if(steige[j][2] != haltestellen[l][0]) continue;
+            ubahnen[linien[i][1] ].haltestellen[hsExists].name = haltestellen[l][3];
+            ubahnen[linien[i][1] ].haltestellen[hsExists].lat = haltestellen[l][6] *1;
+            ubahnen[linien[i][1] ].haltestellen[hsExists].lng = haltestellen[l][7] *1;
+          }
+        }
+        //jede haltestelle hat 2 bahnsteige
+          ubahnen[linien[i][1] ].haltestellen[hsExists]['steig'+steige[j][3]] =
+          steige[j][5];
+
+        // reihenfloge
+        if( steige[j][3] == 'H'){
+          ubahnen[linien[i][1] ].haltestellen[hsExists].reihenfolge = steige[j][4];
+        }
+
+      }
+      //console.log(ubahnen[linien[i][1] ].haltestellen );
+    }
+      //console.log(ubahnen)
+    fs.writeFile('ubahnen_alle.json', JSON.stringify(ubahnen), function(){
+      resolve();
+
+    });
+  });
+}
+
 //HTML für UI
 app.get( '/', function(request, response){
 
 //Import Daten von Wiener Linien
 
 //1. Lade Linien csv
+var dataLinien, dataSteige, dataHaltestellen, dataJSON;
 loadWLData(urlLinienCSV)
-  .then( function(){                  //wenn request erfolgreich ist mach das
+  .then( function(contentCSV){                  //wenn request erfolgreich ist mach das
     console.log('CSV1 geladen');
-    return loadWLData(urlSteigeCSV);
+    //console.log(contentCSV);
+    return parseCSV(contentCSV);
     })
-  .then( function() {
+  .then( function(dataCSV) {
+    dataLinien = dataCSV;
+    console.log('CSV1 Daten geparst');
+    //console.log(dataCSV);
+    return loadWLData(urlSteigeCSV);
+  })
+  .then( function (contentCSV) {
     console.log('CSV2 geladen');
+    return parseCSV(contentCSV);
+  })
+  .then( function (dataCSV) {
+    dataSteige = dataCSV;
+    console.log('CSV2 Daten geparst');
     return loadWLData(urlHaltestellenCSV);
   })
-  .then( function () {
+  .then( function (contentCSV) {
     console.log('CSV3 geladen');
+    return parseCSV(contentCSV);
+  })
+  .then( function (dataCSV) {
+    dataHaltestellen = dataCSV;
+    console.log('CSV3 Daten geparst');
+    return saveWLJSON( dataLinien, dataSteige, dataHaltestellen);
+
+  })
+  .then(function (){
+    console.log('Daten in JSON gespeichert');
+    response.end('FERTIG');
   })
   .catch( function(){console.log('CSV Ladefehler') }); //wenn nicht erfolgreich dies
 
-response.end('FERTIG')
 //2. Parse linien csv
 //3. Lade Bahnsteige CSV
 //4. parse Bahnsteige CSV
 //5. lade haltestellen CSV
 //6. Parse haltestellen CSV
 
+});
+
+app.get( '/json', function(request, response){
+  response.sendFile(
+    __dirname+'/ubahnen_alle.json');
 });
